a9b1799b04f67838c25fc740db6906ae5fdf6242
trak2026z <trak2026z@gmail.com>
2025-08-08 13:54:20 +0200

    Aktualizacja ImageGallery o minimalną logikę pobierania NASA EPIC API

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchTodayImages } from '../services/epicApi';
 
 export const ImageGallery: React.FC> = () => {
-  return (<div>Galheria obrazoo</div>);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchTodayImages()
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <p>Result: {JSON.stringify(data, null, 2)}</p>
+    </div>
+  );
 };
