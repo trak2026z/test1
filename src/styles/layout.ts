0f2e4140c30eecba966e9cdf2e4b4c0fe3fd59ec
trak2026z <trak2026z@gmail.com>
2025-08-08 17:57:56 +0200

    c6

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
-id {
   width: 100%;
   height: 100%;
   display: flex;
@@ -9,17 +8,16 @@ id {
   align-items: center;
   justify-content: center;
   background: ${(emp) => emp.theme.colors.background};
-}
+`;
 
-const StyledWrapper = styled.div
-  {
+const StyledWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: 20px;
   width: 100%;
   max-width: 1200px;
   margin: 0 auto;
-  {(emp) => emp.theme.mix.wrapper}
-};
+  ${(emp) => emp.theme.mix.wrapper};
+`;
 
 export { StyledContainer, StyledWrapper };
