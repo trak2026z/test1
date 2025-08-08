8acb64485da5d4f8299b22ade951f8df7d3ff8ed
trak2026z <trak2026z@gmail.com>
2025-08-08 16:20:26 +0200

    Dodanie pliku theme.ts z definicją motywu aplikacji

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    background: "#f4f4f4",
+    primary: "#0077cc",
+    secondary: "#ff6600",
+    text: "#333",
+  },
+  spacing: (factor: number) => `${factor * 8}px`
+};
