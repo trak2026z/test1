79c2fd650a785debdeaec1989a84fa28efd463e8
trak2026z <trak2026z@gmail.com>
2025-08-08 18:08:16 +0200

    c8

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,12 +1,30 @@
 import { createGlobalStyle, styled } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle `
-body { margin: 0; padding: 0; font-family: 'Arial, sans-serif'; background: ${emp.theme.colors.background}; color: ${emp.theme.colors.text}; box-sizing: border-box; }
-h1, h2, h3, h4, h5, h6 { color: ${emp.theme.colors.primaryText}; }
-a { text-decoration: none; color: inherit; }
-`
+const GlobalStyle = createGlobalStyle`
+  body { 
+    margin: 0; 
+    padding: 0; 
+    font-family: 'Arial, sans-serif'; 
+    background: ${({ theme }) => theme.colors.background}; 
+    color: ${({ theme }) => theme.colors.text}; 
+    box-sizing: border-box; 
+  }
+  h1, h2, h3, h4, h5, h6 { 
+    color: ${({ theme }) => theme.colors.primaryText}; 
+  }
+  a { 
+    text-decoration: none; 
+    color: inherit; 
+  }
+`;
 
-const StyledContainer = styled.div`
-width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; background: ${emp.theme.colors.background};`
+export const StyledContainer = styled.div`
+  width: 100%; 
+  height: 100%; 
+  display: flex; 
+  align-items: center; 
+  justify-content: center; 
+  background: ${({ theme }) => theme.colors.background};
+`;
 
 export default GlobalStyle;
