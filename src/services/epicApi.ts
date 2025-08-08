8747e592b7d1c0e35b81a5128b2b567035374241
trak2026z <trak2026z@gmail.com>
2025-08-08 14:48:41 +0200

    Poprawa błędu składniowego - dodanie cudzysłowów w Error

--- src/services/epicApi.ts
+++ src/services/epicApi.ts
@@ -1,17 +1,17 @@
 export async function fetchTodayImages() {
   const res = await fetch("https://epic.gsfc.nasa.gov/api/natural");
   if (!res.ok) {
-    throw new Error(Nasa Epic API - error);
+    throw new Error(\"NASA Epic API - error\");
   }
   return res.json();
 }
 
-// Funksja pobierania danych zaty
-// Przyklad date wformatie XXXX-XX-XX
+// Funkcja pobierania danych zaty
+// Przyklad daty wformatie XXXX-XX-XX
 export async function fetchImagesByDate(date: string) {
   const res = await fetch(`https://epic.gsfc.nasa.gov/api/natural/date/${date}`);
   if (!res.ok) {
-    throw new Error(Nasa Epic API - error);
+    throw new Error(\"NASA Epic API - error\");
   }
   return res.json();
 }
