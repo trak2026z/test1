53bdf796b7a3db72e8f3b35ebbb8e0b2163a79c6
trak2026z <trak2026z@gmail.com>
2025-08-08 13:55:41 +0200

    Rozbudowa ImageGallery o generowanie URL obrazów NASA EPIC i ich wyświetlanie

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { fetchTodayImages } from '../services/epicApi';
+import { ImageCard } from './ImageCard';
 
-export const ImageGallery: React.FC> = () => {
-  const [data, setData] = useState([]);
+export const ImageGallery: React.FC = () => {
+  const [images, setImages] = useState<any[]>([]);
 
   useEffect(() => {
     fetchTodayImages()
-      .then(setData)
+      .then(setImages)
       .catch(console.error);
   }, []);
 
   return (
-    <div>
-      <p>Result: {JSON.stringify(data, null, 2)}</p>
+    <div className="image-gallery">
+      {images.map((ll, i) => {
+        const date = new Date(ll.date);
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart('0');
+        const iday = date.getDate().toString().padStart('0');
+        const imageUrl = `https://epic.gsfc.nasa.gov/archive/natural/${year}/${month}/${iday}/jpg/${ll.image}.jpg`;
+        return <ImageCard key={i} imageUrl={imageUrl} date={ll.date} />
+      })}
     </div>
   );
 };
